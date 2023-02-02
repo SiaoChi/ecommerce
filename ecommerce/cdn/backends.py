147b9dcbc284692ed91ab42f95c1938dc96f97df
KellyGuo <chichigou@gmail.com>
2023-02-02 16:15:41 +0800

    setting.py -- AWS RDS S3

--- ecommerce/cdn/backends.py
+++ ecommerce/cdn/backends.py
@@ -1,8 +1,8 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-#會在static web的地方創立相對的資料夾
-class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = 'static'
-
-class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = 'media'
+# from storages.backends.s3boto3 import S3Boto3Storage
+#
+# #會在static web的地方創立相對的資料夾
+# class StaticRootS3Boto3Storage(S3Boto3Storage):
+#     location = 'static'
+#
+# class MediaRootS3Boto3Storage(S3Boto3Storage):
+#     location = 'media'
