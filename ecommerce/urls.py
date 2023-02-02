6ffc4b1b85bf9f658c1c2e689dbf03f0edf8157e
KellyGuo <chichigou@gmail.com>
2023-02-02 21:42:41 +0800

    add fly.io dployment code --problem: CSRF token from POST incorrect

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('shop.urls')),
-    path('api/',include('api.urls')),
+    # path('api/',include('api.urls')),
 
 ]
 
